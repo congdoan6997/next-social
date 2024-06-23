79a2086e6f15c0f4dad5a48e48d9852300476f93
PhDBui <congdoan6997@gmail.com>
2024-06-23 10:27:30 +0300

    feat: 🎸 left navbar menu and profile card design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AddPost from "@/components/AddPost";
 import Feed from "@/components/feed/Feed";
+import LeftMenu from "@/components/leftMenu/LeftMenu";
 import RightMenu from "@/components/rightMenu/RightMenu";
 import Stories from "@/components/Stories";
 
@@ -7,8 +8,7 @@ const Homepage = () => {
   return (
     <div className="flex gap-6 pt-6">
       <div className="w-[20%] hidden xl:block">
-        {/* <LeftMenu type="home"/> */}
-        Left menu
+        <LeftMenu type="home" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
